b1b52bcb491b1f9bdcdcc4d1e576ab0b3e2f3baa
unknown <shishlik19@gmail.com>
2023-01-15 20:54:55 +0300

    new update file index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 //    Task 1
+function myFunction (a,b) {
+    let result
+    if (a%2 == 0) {
+        result = a*b;
+    } else {
+       result = a+b;
+    }
+    console.log('Ваш результат: ' + result);
+}
+ 
+myFunction (5,2);
